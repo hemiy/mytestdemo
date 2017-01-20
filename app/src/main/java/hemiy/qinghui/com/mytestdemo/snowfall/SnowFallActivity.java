a9a65a3302bc6e3979421fdb54fd8db5b3225de5
hemiy <hemiy@qq.com>
2017-01-20 14:19:35 +0800

    下雪视图

---
+++ app/src/main/java/hemiy/qinghui/com/mytestdemo/snowfall/SnowFallActivity.java
@@ -0,0 +1,18 @@
+package hemiy.qinghui.com.mytestdemo.snowfall;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import hemiy.qinghui.com.mytestdemo.R;
+
+/**
+ * Created by hemiy on 17/1/20 14:14.
+ */
+
+public class SnowFallActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_snowfall);
+    }
+}
