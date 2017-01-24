c8752d82ea65e02bea23800f32f818d8a12485c5
hemiy <hemiy@qq.com>
2017-01-24 11:16:40 +0800

    刮刮卡效果视图

---
+++ app/src/main/java/hemiy/qinghui/com/mytestdemo/scratchview/ScratchViewActivity.java
@@ -0,0 +1,10 @@
+package hemiy.qinghui.com.mytestdemo.scratchview;
+
+import android.app.Activity;
+
+/**
+ * Created by hemiy on 17/1/24 11:12.
+ */
+
+public class ScratchViewActivity extends Activity {
+}
