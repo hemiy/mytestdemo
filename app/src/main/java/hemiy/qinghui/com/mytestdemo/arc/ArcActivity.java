222d50409299521a79336148100a8f72375fc5da
hemiy <hemiy@qq.com>
2017-05-08 22:02:36 +0800

    builder模式的应用

--- app/src/main/java/hemiy/qinghui/com/mytestdemo/arc/ArcActivity.java
+++ app/src/main/java/hemiy/qinghui/com/mytestdemo/arc/ArcActivity.java
@@ -17,6 +17,8 @@ public class ArcActivity extends Activity {
     @BindView(R.id.ll_report)
     LinearLayout llReport;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +26,7 @@ public class ArcActivity extends Activity {
         ButterKnife.bind(this);
         llReport.removeAllViews();
         //score是百分数，数字、mcolor是外圈进度条以及数字的颜色，unit是百分号，title是数字下面的文字
+
         llReport.addView(new ReportArc(this, 100, 2,"","%") );
     }
 }
